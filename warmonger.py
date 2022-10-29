ffe7bce43fcc3ff502be03a95c2ddddbdfa0a718
Melisa Durmuş <melisa-1347@hotmail.com>
2022-10-29 20:36:36 +0300

    bazı fonksiyonlar eklendi

--- warmonger.py
+++ warmonger.py
@@ -9,27 +9,44 @@ class Faction:
         self.healthpoint = healthpoint
         self.regnumber = regnumber
         self.totalhealth = totalhealth
-        self.flag = flag
+        self.flag = True
     
+    def AssignEnemies(player):
+        print("")
+        if player.name == "Orcs":
+            player.enemypointer1 == 2
+            player.enemypointer2 == 3
+        elif player.name == "Dworves":
+            player.enemypointer1 == 1
+            player.enemypointer2 == 3
+        elif player.name == "Elves":
+            player.enemypointer1 == 1
+            player.enemypointer2 == 2
 
 
     def PrintFaction(self, player):
-        print("zabaduaad")
+
         if self.flag == True:
             status = "Alive"
         else: 
             status = "Defeat"
 
+        d = [
+            ["Faction Name:  ",  self.name],
+            ["Status: ", status],
+            ["Number of Units: ", self.units],
+            ["Attack Point: ", self.attackpoint],
+            ["Health Point: ", self.healthpoint],
+            ["Unit Regen Number: ", self.regnumber],
+            ["Total Faction Health: ", self.totalhealth]
+        ]
+
         if player.name == "Orcs":
-            print(tabulate([
-                ['Status:', status],
-                ['Number of Units', self.units]],
-                headers=['Faction Name:', self.name]))
-            
+            print(tabulate(d)) 
         elif player.name == "Dwarves":
-            print("Dwarves")
+            print(tabulate(d))
         elif player.name == "Elves":
-            print("Elves")
+            print(tabulate(d))
 
 class Orcs(Faction):
 
@@ -43,6 +60,12 @@ class Orcs(Faction):
         self.regnumber = int(input('Please give an regeneration number for the Orcs Faction:'))
         self.totalhealth = self.units * self.healthpoint
         self.flag = True
+    
+    def performAttack():
+        print("")
+    
+    def printorc():
+        print("Stop running, you’ll only die tired!")
 
 class Dwarves(Faction):
     def __init__(self):
@@ -55,6 +78,9 @@ class Dwarves(Faction):
         self.regnumber = int(input('Please give an regeneration number for the Dwarves Faction:'))
         self.totalhealth = self.units * self.healthpoint
         self.flag = True
+    
+    def printdwarves():
+        print("Taste the power of our axes!")
 
 class Elves(Faction):
     def __init__(self):
@@ -66,27 +92,80 @@ class Elves(Faction):
         self.healthpoint = int(input('Please give an health point for the Elves Faction:'))
         self.regnumber = int(input('Please give an regeneration number for the Elves Faction:'))
         self.totalhealth = self.units * self.healthpoint
-        self.flag = True    
+        self.flag = True   
 
-class Merchant():
-    def __init__(self, firstfraction, secondfraction, thirdfraction, weaponpointfirst, armorpointfirst, revenue, weaponpointleft, armorpointleft):
-        self.firstfraction = firstfraction
-        self.secondfraction = secondfraction
-        self.thirdfraction = thirdfraction
-        self.weaponpointfirst = weaponpointfirst
-        self.armorpointfirst = armorpointfirst
-        self.revenue = revenue
-        self.weaponpointleft = weaponpointleft
+    def printelves():
+        print("You cannot reach our elegance.") 
+
+class Merchant(Faction):
+    def __init__(self, firstfaction=0, secondfaction=0, thirdfaction=0, weaponpointfirst=0, armorpointfirst=0, revenue=0, weaponpointleft = 0, armorpointleft = 0):
+        self.firstfaction = firstfaction
+        self.secondfaction = secondfaction
+        self.thirdfaction = thirdfaction
+        self.weaponpointfirst = int(input("Please give a starting weapon point: "))
+        self.armorpointfirst = int(input("Please give an starting armor point: "))
+        self.revenue = 0
+        self.weaponpointleft = weaponpointleft 
         self.armorpointleft = armorpointleft
+
+    def AssignFactions(player):
+        if player.name == "Orcs":
+            player.firstfaction = 1
+            player.secondfaction = 0
+            player.thirdfaction = 0
+        if player.name == "Dworves":
+            player.firstfaction = 0
+            player.secondfaction = 1
+            player.thirdfaction = 0
+        if player.name == "Elves":
+            player.firstfaction = 0
+            player.secondfaction = 0
+            player.thirdfaction = 1
+    
+    def sellWeapons(self ,player, weaponpoint):
+        if player.flag == False:
+            print("The faction you want to sell weapons is dead!")
+        elif weaponpoint > self.weaponpointleft:
+            print("You try to sell more weapons than you have in possession.")
+        else:
+            player.attackpoint += weaponpoint
+            self.weaponpointleft = self.weaponpointleft - weaponpoint
+            print("Weapons Sold!")
+
+    def sellArmors(self ,player, armorpoint):
+        if player.flag == False:
+            print("The faction you want to sell armors is dead!")
+        elif armorpoint > self.armorpointleft:
+            print("You try to sell more armors than you have in possession.")
+        else:
+            player.regnumber += armorpoint
+            self.armorpointleft = self.armorpointleft - armorpoint
+            print("Armors Sold!")
+    
+    def EndTurn(self):
+        self.weaponpointleft = self.weaponpointfirst
+        self.armorpointleft = self.armorpointfirst
+        
+
 
 def Game (): 
 
     msg = "Welcome to The Warmonger Game"
     print(msg)
-    player1 = Orcs()
-    player2 = Dwarves()
+    #player1 = Orcs()
+    #player2 = Dwarves()
     player3 = Elves()
-    player1.PrintFaction(player1)
+    merchant = Merchant()
+    merchant.weaponpointleft = merchant.weaponpointfirst
+    merchant.armorpointleft = merchant.armorpointfirst
+    player3.PrintFaction(player3)
+    print(merchant.weaponpointleft)
+    Merchant.sellWeapons(merchant,player3,20)
+    player3.PrintFaction(player3)
+    Merchant.sellWeapons(merchant,player3,20)
+    print(merchant.weaponpointleft)
+    Merchant.sellWeapons(merchant,player3,20)
+    print(merchant.weaponpointleft)
 
 Game()
 
